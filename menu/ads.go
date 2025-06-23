1d254f4f0c26859b5d4eaec46bbfd1e98250ff1b
ranandasatria <rananda24@gmail.com>
2025-06-23 23:13:20 +0700

    refactor: move ads channel to menu directory

---
+++ menu/ads.go
@@ -0,0 +1,24 @@
+package menu
+
+import (
+	"math/rand"
+	"time"
+)
+
+func AdsChannel() chan string {
+	ch := make(chan string)
+	go func() {
+		rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+		ads := []string{
+			"Coba Nasi Goreng Kampung, pedasnya mantap!\n",
+			"Es Teh Kampul segar buat temen makan!\n",
+			"Pisang Goreng enak buat ngemil!\n",
+			"Pudding Coklat manisnya pas!\n",
+		}
+		for {
+			ch <- ads[rng.Intn(len(ads))]
+			time.Sleep(3 * time.Second)
+		}
+	}()
+	return ch
+}
